b333c3164441f8bfd6930aefeecc2398087155f1
Alexander Myska <amyska04@iastate.edu>
2025-05-27 17:03:15 -0500

    Added Plasma and Erratic Jokers, fixed Anaglyph Joker
    Fully implemented every Joker in the mod. Time for playtesting, bug finding, and better art!

--- main.lua
+++ main.lua
@@ -659,7 +659,7 @@ SMODS.Joker {
     end,
 
     calculate = function(self, card, context)
-        if context.context == "eval" and G.GAME.last_blind and G.GAME.last_blind.boss then
+        if context.end_of_round and not context.game_over and context.main_eval and G.GAME.blind.boss then
             G.E_MANAGER:add_event(Event({
                 func = function()
                     add_tag(Tag("tag_double"))
@@ -679,14 +679,53 @@ SMODS.Joker {
         text = {
             "Balance {C:blue}Chips{} and",
             "{C:red}Mult{} when calculating",
-            "score for played hand"
+            "score for played hand",
+            "{C:inactive}(On Plasma Deck, gives",
+            "{X:red,C:white}X#1#{}{C:inactive} Mult)"
         },
         unlock = {
             "Win a run with the",
             "{C:attention}Plasma Deck"
         }
     },
-    
+    config = {
+        extra = {
+            Xmult = 3
+        }
+    },
+    rarity = 3,
+    pos = { x = 3, y = 2 },
+    atlas = "jokers",
+    cost = 8,
+    unlocked = false,
+    unlock_condition = {type = "win_deck", deck = "b_plasma"},
+    blueprint_compat = false,
+    eternal_compat = true,
+    perishable_compat = true,
+
+    loc_vars = function(self, info_queue, card)
+        return {
+            vars = {
+                card.ability.extra.Xmult
+            }
+        }
+    end,
+
+    calculate = function(self, card, context)
+        if G.GAME.selected_back:get_name() == "Plasma Deck" then
+            if context.joker_main then
+                return {
+                    x_mult = card.ability.extra.Xmult
+                }
+            end
+        else
+            if context.final_scoring_step then
+                return {
+                    balance = true
+                }
+            end
+        end
+    end
 }
 
 SMODS.Joker {
@@ -703,5 +742,45 @@ SMODS.Joker {
             "{C:attention}Erratic Deck"
         }
     },
-    
+    rarity = 3,
+    pos = { x = 4, y = 2 },
+    atlas = "jokers",
+    cost = 8,
+    unlocked = false,
+    unlock_condition = {type = "win_deck", deck = "b_erratic"},
+    blueprint_compat = false,
+    eternal_compat = true,
+    perishable_compat = true,
+
+    loc_vars = function(self, info_queue, card)
+        return {
+            vars = {
+            }
+        }
+    end,
+
+    calculate = function(self, card, context)
+        if context.after then
+            local _suit = pseudorandom_element({"Spades", "Hearts", "Clubs", "Diamonds"}, pseudoseed("err"))
+            local _rank = math.random(2, 14)
+            for i = 1, #context.scoring_hand do
+                G.E_MANAGER:add_event(Event({trigger = "after", delay = 0.1, func = function()
+                    local _card = context.scoring_hand[i]
+                    _card:flip()
+                    local suit_prefix = string.sub(_suit, 1, 1).."_"
+                    local rank_suffix = _rank
+                    if rank_suffix < 10 then rank_suffix = tostring(rank_suffix)
+                    elseif rank_suffix == 10 then rank_suffix = "T"
+                    elseif rank_suffix == 11 then rank_suffix = "J"
+                    elseif rank_suffix == 12 then rank_suffix = "Q"
+                    elseif rank_suffix == 13 then rank_suffix = "K"
+                    elseif rank_suffix == 14 then rank_suffix = "A"
+                    end
+                    _card:set_base(G.P_CARDS[suit_prefix..rank_suffix])
+                    _card:flip()
+                return true end }))
+            end
+            delay(1.5)
+        end
+    end
 }
